97de442374609cd47088712b56693b12ba4c5636
zzw <1464003642@qq.com>
2022-11-09 12:14:18 +0800

    t

---
+++ public/store/config.js
@@ -0,0 +1,6 @@
+window.publicConfig = {
+  // 系统名称
+  APP_NAME: 'https://coolart.space/index.php?s=/store',
+  BASE_URL: 'https://coolart.space',
+  BASE_API: 'https://coolart.space/index.php?s=/store'
+}
