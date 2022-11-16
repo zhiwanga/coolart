49e67b177898452598a39c078e131fe8e27dbf51
zzw <1464003642@qq.com>
2022-11-16 17:21:56 +0800

    t

--- app/store/controller/Rate.php
+++ app/store/controller/Rate.php
@@ -15,14 +15,60 @@ class Rate extends Controller
     {
         $input = input();
 
-        $insert = [
-            'withdrawal_rate'   => $input['withdrawal_rate'],
-            'transac_rate'      => $input['transac_rate'],
-            'remarks'           => $input['remarks'],
-            'create_time'       => time()
-        ];
-        Db::name('rate_control')->insert($insert);
+        try {
+            $insert = [
+                'withdrawal_rate'   => $input['withdrawal_rate'],
+                'transac_rate'      => $input['transac_rate'],
+                'remarks'           => $input['remarks'],
+                'create_time'       => time()
+            ];
+            Db::name('rate_control')->insert($insert);
+            return $this->renderSuccess([], 'success');
+        } catch (\Throwable $th) {
+            return $this->renderError($th->getMessage());
+        }
+    }
 
-        return $this->renderSuccess([], 'success');
+    public function update()
+    {
+        $input = input();
+
+        try {
+            $update = [
+                'withdrawal_rate'   => $input['withdrawal_rate'],
+                'transac_rate'      => $input['transac_rate'],
+                'remarks'           => $input['remarks'],
+                'update_time'       => time()
+            ];
+            $res = Db::name('rate_control')->where('id', $input['id'])->update($update);
+            if($res) {
+                return $this->renderSuccess([], 'success');
+            }else{
+                return $this->renderError('更新失败');
+            }
+        } catch (\Throwable $th) {
+            return $this->renderError($th->getMessage());
+        }
+    }
+
+    public function delete()
+    {
+        $input = input();
+        try {
+            $res = Db::name('rate_control')->where('id', $input['id'])->delete();
+            if($res) {
+                return $this->renderSuccess([], 'success');
+            }else{
+                return $this->renderError('删除失败');
+            }
+        } catch (\Throwable $th) {
+            return $this->renderError($th->getMessage());
+        }
+    }
+
+    public function list()
+    {
+        $list = Db::name('rate_control')->field('id, withdrawal_rate, transac_rate, remarks, from_unixtime(create_time,"%Y-%m-%d %H:%i") create_time, from_unixtime(update_time,"%Y-%m-%d %H:%i") update_time')->select()->toArray();
+        return $this->renderSuccess($list, 'success');
     }
 }
