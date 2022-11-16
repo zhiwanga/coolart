7a82b6dad0e8c07e6fe65e963fcdc977599dec11
zzw <1464003642@qq.com>
2022-11-16 13:27:02 +0800

    t

--- app/store/model/Goods.php
+++ app/store/model/Goods.php
@@ -286,6 +286,12 @@ class Goods extends GoodsModel
      */
     private function createData(array $data): array
     {
+        if($data['cover_id']) {
+            $data['cover_path'] = Db::name('upload_file')->where('file_id', $data['cover_id']['0'])->value('file_path');
+        }else{
+            unset($data['cover_id']);
+            $data['cover_path'] = '';
+        }
         // 默认数据
         $data = array_merge($data, [
             'line_price' => $data['line_price'] ?? 0,
