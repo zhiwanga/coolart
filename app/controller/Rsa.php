c5ab9cc092b1942c480e12a75521e63784a1475f
zzw <1464003642@qq.com>
2022-11-16 11:07:31 +0800

    t

--- app/controller/Rsa.php
+++ app/controller/Rsa.php
@@ -12,9 +12,8 @@ class Rsa
      * @return string     
      */   
     private static function getPrivateKey() 
-    {        
-        $abs_path = '/www/wwwroot/serve_test/rsa_private_key.pem';
-        // $abs_path = public_path().'..\rsa_private_key.pem';
+    {
+        $abs_path = root_path().'rsa_private_key.pem';
         $content = file_get_contents($abs_path);    
         return openssl_pkey_get_private($content);    
     }
@@ -24,9 +23,8 @@ class Rsa
      * @return string
      */   
     private static function getPublicKey()
-    {   
-        $abs_path = '/www/wwwroot/serve_test/rsa_public_key.pem';
-        // $abs_path = public_path().'..\rsa_public_key.pem';
+    {
+        $abs_path = root_path().'rsa_public_key.pem';
         $content = file_get_contents($abs_path);    
         return openssl_pkey_get_public($content);
     }
@@ -37,50 +35,50 @@ class Rsa
      * @return null|string     
      */   
     public static function privEncrypt($data = '')    
-    {        
-        if (!is_string($data)) {            
-            return null;       
-        }        
+    {
+        if (!is_string($data)) {
+            return null;
+        }
         return openssl_private_encrypt($data,$encrypted, self::getPrivateKey()) ? base64_encode($encrypted) : null;    
     }
 
     /**
      * 公钥加密     
-     * @param string $data     
-     * @return null|string     
+     * @param string $data
+     * @return null|string
      */   
-    public static function publicEncrypt($data = '')   
-    {        
-        if (!is_string($data)) {            
-            return null;        
-        }        
+    public static function publicEncrypt($data = '')
+    {
+        if (!is_string($data)) {
+            return null;
+        }
         return openssl_public_encrypt($data,$encrypted,self::getPublicKey()) ? base64_encode($encrypted) : null;    
     }
 
-    /**     
-     * 私钥解密     
-     * @param string $encrypted     
-     * @return string     
-     */   
-    public static function privDecrypt($encrypted = '')    
-    {        
+    /**
+     * 私钥解密
+     * @param string $encrypted
+     * @return string
+     */
+    public static function privDecrypt($encrypted = '')
+    {
         if (!is_string($encrypted)) {            
-            return null;        
+            return null;
         }
         return (openssl_private_decrypt(base64_decode($encrypted), $decrypted, self::getPrivateKey())) ? $decrypted : null;    
     }
 
-    /**     
-     * 公钥解密     
-     * @param string $encrypted     
-     * @return null     
+    /**
+     * 公钥解密
+     * @param string $encrypted
+     * @return null
      */
-    public static function publicDecrypt($encrypted = '')    
+    public static function publicDecrypt($encrypted = '')
     {        
-        if (!is_string($encrypted)) {            
-            return null;        
+        if (!is_string($encrypted)) {
+            return null;
         }
-        return (openssl_public_decrypt(base64_decode($encrypted), $decrypted, self::getPublicKey())) ? $decrypted : null;    
+        return (openssl_public_decrypt(base64_decode($encrypted), $decrypted, self::getPublicKey())) ? $decrypted : null;
     }
 
     /**
--- app/controller/market/Observation.php
+++ app/controller/market/Observation.php
@@ -4,10 +4,7 @@ declare (strict_types = 1);
 namespace app\controller\market;
 
 use app\model\market\RealtGoods;
-use app\api\service\User as UserService;
-use app\common\service\Transaction;
 use app\controller\Rsa;
-use think\facade\Db;
 
 class Observation
 {
@@ -30,20 +27,7 @@ class Observation
     }
     public function test()
     {
-        $arr = [
-            0 => ['coll_id' => 1, 'aa' => 1, 'bb' => 1],
-            1 => ['coll_id' => 2, 'aa' => 2, 'bb' => 2],
-            2 => ['coll_id' => 3, 'aa' => 3, 'bb' => 3],
-            3 => ['coll_id' => 1, 'aa' => 4, 'bb' => 4],
-        ];
-        $r= $this->array_unique_fb($arr);
-        var_dump($r);die;
-    }
-    public function array_unique_fb($array2D=[]){
-        $temp = [];
-        foreach ($array2D as $k=>$v){
-            $temp[$v['coll_id']] = $v;
-        }
-        return array_values($temp);
+        $res = Rsa::privEncrypt('asdasd');
+        var_dump($res);die;
     }
 }
